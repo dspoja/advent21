e1c9db19fc35850b861322211a644b36e3119427
dspoja <danjaspoja@gmail.com>
2021-12-02 16:11:06 -0500

    Day 2 solution

--- advent2.py
+++ advent2.py
@@ -1,8 +1,40 @@
-def navigate():
-    final_depth = 0
-    with open('input2-sample', "rb") as directions:
+def navigate_part1() -> int:
+    with open('input2', "rb") as inputs:
+        horizontal = 0
+        vertical = 0
+        for direction_input in inputs:
+            direction = direction_input.split()
+            name = direction[0].decode("utf-8")
+            num_units = int(direction[1])
+            if name == "forward":
+                horizontal += num_units
+            if name == "up":
+                vertical -= num_units
+            if name == "down":
+                vertical += num_units
+        return horizontal * vertical
 
-        for direction in directions:
-            pass
 
-    print(f"Final depth is {final_depth}")
+def navigate_part2() -> int:
+    with open('input2', "rb") as inputs:
+        horizontal = 0
+        vertical = 0
+        aim = 0
+        for direction_input in inputs:
+            direction = direction_input.split()
+            name = direction[0].decode("utf-8")
+            num_units = int(direction[1])
+            if name == "forward":
+                horizontal += num_units
+                vertical += aim * num_units
+            if name == "up":
+                aim -= num_units
+            if name == "down":
+                aim += num_units
+        return horizontal * vertical
+
+final_depth = navigate_part1()
+print(f"Final depth 1 is {final_depth}")
+
+final_depth = navigate_part2()
+print(f"Final depth 2 is {final_depth}")
