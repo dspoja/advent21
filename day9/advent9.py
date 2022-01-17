223ae7f3d78ebd38bde203de95f9bb1b51139ee9
Danja Spoja <9200064+dspoja@users.noreply.github.com>
2022-01-16 19:11:54 -0500

    Cleanup day 9 part 1

--- day9/advent9.py
+++ day9/advent9.py
@@ -1,47 +1,42 @@
 def exists(matrix: list, row: int, col: int, row_next: int, col_next: int) -> bool:
-    global rows_num, columns_num
-    if row_next < 0 or col_next < 0 or row_next > rows_num or col_next > columns_num:
+    if row_next < 0 or col_next < 0 or row_next > (len(matrix) - 1) or col_next > (len(matrix[0]) - 1):
         return True
     if matrix[row][col] < matrix[row_next][col_next]:
         return True
     return False
 
 
-def find_low(row: int, col: int, matrix: list) -> list:
+def find_low(row: int, col: int, matrix: list) -> int:
     '''
         right i,j+1
         left i,j-1
         up: i-1, j
         down: i+1, j
     '''
-    lows = []
     if (exists(matrix, row, col, row, col+1) and
         exists(matrix, row, col, row, col-1) and
         exists(matrix, row, col, row-1, col) and
         exists(matrix, row, col, row+1, col)):
-        lows.append(matrix[row][col])
+        return matrix[row][col]
 
-    return lows
+    return None
 
 
 def part1() -> int:
-    global rows_num, columns_num
-    with open("input9-sample", "rb") as data:
+    with open("input9", "rb") as data:
         matrix = [list(map(int,line.decode("utf8").split()[0])) for line in data]
-    #scan through each row and find low points
-    rows_num = len(matrix) - 1
-    columns_num = len(matrix[0]) - 1
+    # scan through each row and find low points
     lows = []
     for i, row in enumerate(matrix):
         for j, _ in enumerate(row):
-            lows.extend(find_low(i, j, matrix))
+            low = find_low(i, j, matrix)
+            if low is not None:
+                lows.append(low)
 
-    sum = 0
+    low_sum = 0
     for low in lows:
-        sum = sum + low + 1
-    return sum
+        low_sum = low_sum + low + 1
+    return low_sum
 
 
-rows_num = 0
-columns_num = 0
 print(part1())
