447a06d8259e23f27167d280c52dcc86cd6165d1
Danja Spoja <9200064+dspoja@users.noreply.github.com>
2022-01-16 19:48:09 -0500

    Part 2 of day 9

--- day9/advent9.py
+++ day9/advent9.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def exists(matrix: list, row: int, col: int, row_next: int, col_next: int) -> bool:
     if row_next < 0 or col_next < 0 or row_next > (len(matrix) - 1) or col_next > (len(matrix[0]) - 1):
         return True
@@ -22,21 +25,61 @@ def find_low(row: int, col: int, matrix: list) -> int:
     return None
 
 
-def part1() -> int:
+def find_basin_size(row: int, col: int, matrix: list) -> int:
+    '''
+    use floodfill technique
+    '''
+    stack=[(row, col)]
+    basin_values = []
+    while stack:
+        i, j = stack.pop()
+        if matrix[i][j] != 9:
+            if (i, j) not in basin_values:
+                basin_values.append((i, j))
+            else:
+                continue
+            if i + 1 < len(matrix):
+                stack.append((i + 1, j))
+            if i - 1 >= 0:
+                stack.append((i - 1, j))
+            if j + 1 < len(matrix[0]):
+                stack.append((i, j + 1))
+            if j - 1 >= 0:
+                stack.append((i, j - 1))
+        else:
+            pass
+    return len(basin_values)
+
+
+def compute_smoke_basins(compute_basin_size: bool=False) -> int:
     with open("input9", "rb") as data:
         matrix = [list(map(int,line.decode("utf8").split()[0])) for line in data]
     # scan through each row and find low points
-    lows = []
+    basins = []
+    basin_sizes = deque()
     for i, row in enumerate(matrix):
         for j, _ in enumerate(row):
             low = find_low(i, j, matrix)
             if low is not None:
-                lows.append(low)
+                basins.append(low)
+                if compute_basin_size:
+                    # compute basin for this point
+                    basin_size = find_basin_size(i, j, matrix)
+                    print(f"basin size={basin_size}")
+                    basin_sizes.append(basin_size)
+    if not compute_basin_size:
+        low_sum = 0
+        for low in basins:
+            low_sum = low_sum + low + 1
+        return low_sum
+    else:
+        basin_sizes = sorted(basin_sizes,reverse=True)
+        total = 1
+        end = 3 if len(basin_sizes) > 3 else len(basin_sizes)
+        for i in range(0, end):
+            total *= basin_sizes[i]
+        return total
 
-    low_sum = 0
-    for low in lows:
-        low_sum = low_sum + low + 1
-    return low_sum
 
-
-print(part1())
+print(compute_smoke_basins())
+print(compute_smoke_basins(compute_basin_size=True))
